77f01a099cd671f45dfcc8746d3215848e122e51
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-18 12:28:34 +0700

    refactor: rename example project to `bonaccis`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/bonacci/__main__.py
+++ src/bonacci/__main__.py
@@ -5,10 +5,10 @@ from . import fibonacci_sequence
 
 def main() -> None:
     parser = argparse.ArgumentParser(
-        prog="my_fibonacci",
-        description="Generate a Fibonacci sequence up to the given number of terms.",
+        prog="bonacci",
+        description="Generate a Fibonacci sequence up to the given number of terms",
     )
-    parser.add_argument("--version", action="version", version="0.0.0")
+    parser.add_argument("-v", "--version", action="version", version="0.0.0")
     parser.add_argument("n", type=int, help="The number of terms")
     args = parser.parse_args()
 
